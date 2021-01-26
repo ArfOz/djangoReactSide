ff3e387b7f34417d3c7a7bceac03c35c3af67579
ARIF OZKAN <arfoz1245@gmail.com>
2021-01-26 18:08:31 +0300

    blog added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignInSide from "./src/Login/SignInSide"
 import SignUp from "./src/Register/SignUp"
+import Blog from "./src/BlogPage/Blog"
+
+
+
+
 const App = () => {
     return (
         <div>
          <SignInSide />
          <SignUp />
+         <Blog />
          
         </div>
     )

603563b0dee4f31fce952196878fd25a576c438f
ArfOz <arfoz1245@gmail.com>
2021-01-26 16:50:51 +0300

    react side initiated

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import SignInSide from "./src/Main"
+const App = () => {
+    return (
+        <div>
+         <SignInSide/>
+        </div>
+    )
+}
+export default App;

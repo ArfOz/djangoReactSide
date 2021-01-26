12282f9c07803dd53384a19d321b5ef8a381cfd0
ArfOz <arfoz1245@gmail.com>
2021-01-26 17:02:42 +0300

    signin page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import SignInSide from "./src/Main"
+import SignInSide from "./src/Login/Main"
 const App = () => {
     return (
         <div>
